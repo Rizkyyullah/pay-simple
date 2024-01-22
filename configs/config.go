64747bf4c465bb4bfca0de80781483525c776cf5
rizkyyullah <rizkyyullah1945@gmail.com>
2024-01-23 06:52:03 +0700

    [ADD] Add function to create a .env file when deploying

--- configs/config.go
+++ configs/config.go
@@ -1,7 +1,10 @@
 package configs
 
 import (
+  "fmt"
+  "io"
   "log"
+  "os"
   "time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -39,6 +42,50 @@ func LoadConfig() TokenConfig {
 	
 	if err := viper.Unmarshal(&ENV); err != nil {
 	  log.Fatal("configs.Unmarshal Err :", err)
+	}
+	
+	if os.Getenv("APPMODE") == "DEPLOY" {
+	  file, err := os.Create(".env")
+    if err != nil {
+        log.Fatal("Error :", err)
+    }
+    defer file.Close()
+
+    // teks := "Ini adalah contoh menulis ke file menggunakan bahasa Golang.\n"
+    // data := []byte(teks)
+    // _, err = file.Write(data)
+    // if err != nil {
+    //     fmt.Println("Error:", err)
+    //     os.Exit(1)
+    // }
+
+    text := `
+DB_HOST=$DB_HOST
+DB_PORT=$DB_PORT
+DB_USER=$DB_USER
+DB_PASSWORD=$DB_PASSWORD
+DB_NAME=$DB_NAME
+API_PORT=$API_PORT
+PORT=$PORT
+TOKEN_ISSUE=$TOKEN_ISSUE
+TOKEN_SECRET=$TOKEN_SECRET
+TOKEN_EXPIRE=$TOKEN_EXPIRE
+TIMEZONE=$TIMEZONE
+  `
+    _, err = io.WriteString(file, text)
+    if err != nil {
+        log.Fatal("Error :", err)
+    }
+
+    // Menyimpan perubahan ke file
+    err = file.Sync()
+    // Jika terjadi error, tampilkan pesan error dan keluar dari program
+    if err != nil {
+        log.Fatal("Error :", err)
+    }
+
+    // Menampilkan pesan sukses
+    fmt.Println("Sukses menulis ke file.")
 	}
 	
 	return TokenConfig{
