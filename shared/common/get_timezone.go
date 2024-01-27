47c198cfef55ba8958d4814f22fccd0f7ce37b31
rizkyyullah <rizkyyullah1945@gmail.com>
2024-01-27 11:53:21 +0700

    [FIX] Fix timezone not detected when deploying #iii

--- shared/common/get_timezone.go
+++ shared/common/get_timezone.go
@@ -3,22 +3,9 @@ package common
 import (
   "fmt"
   "time"
-
-  "github.com/spf13/viper"
 )
 
 func GetTimezone() *time.Location {
-  var location *time.Location
-  viper.AutomaticEnv()
-
-  envLocation := viper.GetString("TIMEZONE_IDN")
-  if envLocation == "" {
-    location, _ = time.LoadLocation("Asia/Jakarta")
-  } else {
-    location, _ = time.LoadLocation(envLocation)
-  }
-
-  fmt.Println(envLocation)
-
+  location, _ := time.LoadLocation("Asia/Jakarta")
   return location
 }
